d4aa27c1117c526f5c77ca6274fa674af9f4771b
mobilemessenger <49979951+mobilemessenger@users.noreply.github.com>
2019-05-02 00:15:21 +0300

    Add files via upload

---
+++ Trapeze.java
@@ -0,0 +1,10 @@
+package myPrivate;
+
+public class Trapeze {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Triangle.java
@@ -0,0 +1,18 @@
+package myPrivate;
+
+public class Triangle {
+	   int a,b, sq;
+	   String color;
+	 
+	   	   Triangle(int a, int b, String color){
+		   this.a=a;
+		   this.b=b;
+		   this.color=color;
+		   sq=(a*b)/2;
+	   }
+	   	   
+	 
+
+	}
+
+
